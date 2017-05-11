6fd240a78bb0d943cbe48da3194840023a1fb848
Guillermo Sosa <nbkguille@hotmail.com>
2017-05-10 22:15:48 -0300

    Nuevo

--- Procedimientos/TP3_02_SumarDias.vb
+++ Procedimientos/TP3_02_SumarDias.vb
@@ -1,9 +1,10 @@
 ﻿Module TP3_02_SumarDias
+    'Crear una función que reciba una fecha y un entero que representa una cantidad de días a sumar
+    'a la fecha. Desde el módulo principal ingresar una fecha y la cantidad de días e invocar a esta
+    'función pasándole los parámetros: fecha actual y días, mostrar el valor obtenido.
     Sub Main()
-        'Declaracion de variables
         Dim fecha As Date
         Dim dias As UShort
-        'Ingreso de datos
         Console.WriteLine("Sumar días a la fecha")
         Console.WriteLine()
         Console.Write("Ingrese fecha: ")
